c6b75624509c572e559163b2175855cfe8eda02f
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-16 09:45:55 -0500

    Create python_wk1_challenge.py

---
+++ wk1/python_wk1_challenge.py
@@ -0,0 +1,11 @@
+# Spicy Challenge: Swap two variables!
+
+var_a = 10
+var_b = 20
+
+# Write your code here to swap var_a and var_b.
+# Hint: Create a temporary variable, like this: temp = var_a
+
+# When you run the program, the output should show the values have swapped.
+print(f"var_a is now {var_a}")
+print(f"var_b is now {var_b}")
