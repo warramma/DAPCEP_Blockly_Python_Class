7f7ad817a9f06c2acaa94ac15d9e60aa1c6b2dc7
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-16 09:53:18 -0500

    Create python_wk3.py

---
+++ wk3/python_wk3.py
@@ -0,0 +1,22 @@
+# Loops are for repetition!
+
+# Part 1: For Loop
+# Use a for loop to print "Hello" 5 times.
+# Uncomment the for loop ⬇️⬇️⬇️
+# for i in _______________:
+#     print("Hello")
+
+# Part 2: While Loop
+# A simple guessing game.
+secret_number = 7
+tries = 3
+guess = 0
+
+while True: #update this so it checks if there are tries remaining
+    # Get user input.
+    guess = int(input("Guess the secret number: "))
+
+    if True: ##check if the guess is right!
+      print('You got it!')
+      break
+    #now add TWO conditions to check if the guess is too LOW & if the guess is too HIGH
