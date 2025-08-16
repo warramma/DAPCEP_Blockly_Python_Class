d76c8f3a1281377b1a681e72765be6e6dd724413
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-16 09:54:47 -0500

    Create python_wk4.py

---
+++ wk4/python_wk4.py
@@ -0,0 +1,20 @@
+# Part 1: Functions
+# Create a function that prints a personalized greeting.
+
+def greet_user(name):
+    # Fill in the code to print "Hello, [name]!"
+    _____________________________________
+
+greet_user("_______________") # Call the function here.
+
+# Part 2: Lists
+# Create a list of your 3 favorite things.
+favorite_animals = [
+    "dog",
+    "cat",
+    "_______________"
+]
+
+# Use a for loop to print each animal from the list.
+for item in ________________:
+    print(item)
