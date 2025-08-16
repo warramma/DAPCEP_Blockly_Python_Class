267ca1a7d0c211a13a33158cd9f2018cb3ff6f01
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-16 09:46:35 -0500

    Create python_wk2.py

---
+++ wk2/python_wk2.py
@@ -0,0 +1,13 @@
+# Conditionals and Decisions!
+
+# Get a number from the user.
+user_number = int(input("Enter a number: "))
+
+# Use if/elif/else to check the number.
+if user_number > 10:
+    print("That's a big number!")
+# Replace the "True" after elif in the next condition to ACTUALLY check if the number is between 1 and 10.
+elif True:
+    print("That's a number between 1 and 10.")
+else:
+    print("That's a very small number.")
