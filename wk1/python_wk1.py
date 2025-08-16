c95e1d1ad4bfebaacbb172fd1bc83ba15b5888e9
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-16 09:45:18 -0500

    Create python_wk1.py

---
+++ wk1/python_wk1.py
@@ -0,0 +1,11 @@
+# My first Python program!
+# Translate your Blockly variable exercise into Python.
+
+# Fill in your name as a string (text in quotes).
+my_name = ""
+
+# Fill in your age as an integer (a whole number).
+my_age = None
+
+# This program will print a sentence using your variables.
+print(f"My name is {my_name} and I am {my_age} years old.")
