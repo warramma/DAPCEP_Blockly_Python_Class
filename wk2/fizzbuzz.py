918b917bca2517143e16e464da4b95bcf7dc68ad
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-16 09:47:49 -0500

    Create fizzbuzz.py

---
+++ wk2/fizzbuzz.py
@@ -0,0 +1,20 @@
+# This loop will go through every number from 1 to 15.
+for number in range(1, 16):
+    # This is the "FizzBuzz" condition.
+    # Check if the number is a multiple of both 3 AND 5.
+    if True:
+        print("FizzBuzz")
+
+    # This is the "Fizz" condition.
+    # Check if the number is a multiple of just 3.
+    elif True:
+        print("Fizz")
+
+    # This is the "Buzz" condition.
+    # Check if the number is a multiple of just 5.
+    elif True:
+        print("Buzz")
+
+    # If none of the above conditions are true, print the number.
+    else:
+        print(number)
