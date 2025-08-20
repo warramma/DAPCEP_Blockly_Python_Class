61b23023398168a084680e1bd0bf29b9c33a8a82
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-19 22:23:10 -0500

    Update python_wk2_challenge.py

--- wk2/python_wk2_challenge.py
+++ wk2/python_wk2_challenge.py
@@ -1,6 +1,7 @@
 number = 50
 
-# Check if the number is even.
+# Check if the number is even. This conditions uses the modulo operator (%) which checks for any remainders!
+# if a number is a multiple of another number, it's remainder is 0 (zero)
 if number % 2 == 0:
     print(f"{number} is an even number.")
     # Now, inside this if statement, check if it's also a multiple of 10. Replace "True" with the correct condition
