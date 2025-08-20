8ef9ecf7bcf0dfc7ef5445e0c1601d7f51b67170
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-19 20:38:52 -0500

    Update python_wk1.py

--- wk1/python_wk1.py
+++ wk1/python_wk1.py
@@ -1,11 +1,10 @@
 # My first Python program!
 # Translate your Blockly variable exercise into Python.
 
-# Fill in your name as a string (text in quotes).
-my_name = ""
+# create a name variable and put your name in quotes for a string!
 
-# Fill in your age as an integer (a whole number).
-my_age = None
+
+# create an age variable and enter your age.
 
 # This program will print a sentence using your variables.
-print("My name is ", my_name, " and I am ", my_age, " years old.")
+print("My name is ", your_name_variable, " and I am ", your_age_variable, " years old.")
