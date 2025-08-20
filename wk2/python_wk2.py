cfb65a4d964d071b81cd50afd4b99e74d0d50016
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-19 22:20:50 -0500

    Update python_wk2.py

--- wk2/python_wk2.py
+++ wk2/python_wk2.py
@@ -4,7 +4,9 @@
 user_number = int(input("Enter a number: "))
 
 # Use if/elif/else to check the number.
-if user_number > 10:
+
+#update the if condition to check if the number if greater than 10
+if True:
     print("That's a big number!")
 # Replace the "True" after elif in the next condition to ACTUALLY check if the number is between 1 and 10.
 elif True:
